c90cd6a9cdb05c212b6761b4725c089eeef8df63
MokryiYurij <y.mokryi@pg-mc.com>
2021-10-01 15:07:29 +0300

    add customer controller

---
+++ myapp/src/Controller/CustomerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomerController extends AbstractController
+{
+    /**
+     * @Route("/customer", name="customer")
+     */
+    public function index(): Response
+    {
+        return $this->render('customer/index.html.twig', [
+            'controller_name' => 'CustomerController',
+        ]);
+    }
+}
