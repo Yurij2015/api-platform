7f3cafbc8dd3dbb0fff5fe76680ae51cc581bc59
MokryiYurij <y.mokryi@pg-mc.com>
2021-10-04 20:40:12 +0300

    set admin main page

--- myapp/src/Controller/Admin/DashboardController.php
+++ myapp/src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Customer;
@@ -17,7 +18,10 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+//        return parent::index();
+        $routerBuilder = $this->get(AdminUrlGenerator::class);
+        $url = $routerBuilder->setController(CustomerCrudController::class)->generateUrl();
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
